eef3d79e7baf0e7ddd23cb04215bb05adc2e0275
mbubula6 <mbubula6@gmail.com>
2026-03-21 03:56:52 +0100

    made functioning getting user's data (again)

--- scripts/main.py
+++ scripts/main.py
@@ -2,18 +2,16 @@ import pandas as pd
 from supabase import create_client
 from sklearn.ensemble import RandomForestClassifier
 
-# 1. Setup Connection
 url = "https://euinlbrrdvzqeaniakyo.supabase.co"
 key = ""
 supabase = create_client(url, key)
-
 
 def get_user_data(user_uuid):
     """
     Fetches both data and preferences for a specific ID.
     """
     try:
-        response = supabase.table("user_data").select("*").execute()
+        response = supabase.table("client_order").select("*, user_data(*)").eq("id_client",user_uuid).execute()
 
         return response.data
 
@@ -22,7 +20,7 @@ def get_user_data(user_uuid):
         return None
 
 
-target_uuid = "39908fca-7292-4ed3-b5c1-e9fecd28d33d"
-user_info = get_user_data(target_uuid)
+target_user_id = "03ec4093-a950-48ea-a95e-7459b945aeda"
+user_info = get_user_data(target_user_id)
 print(user_info)
 
